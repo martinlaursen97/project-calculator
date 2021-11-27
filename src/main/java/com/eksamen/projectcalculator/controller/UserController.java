ffe69d9a4f93cf4660382b5c9cfcd2f05e694290
martinlaursen97 <martin_laursen21@hotmail.com>
2021-11-27 23:01:50 +0100

    gantt diagram (can now load task objects into gantt diagram)

--- src/main/java/com/eksamen/projectcalculator/controller/UserController.java
+++ src/main/java/com/eksamen/projectcalculator/controller/UserController.java
@@ -1,12 +1,18 @@
 package com.eksamen.projectcalculator.controller;
 
 import com.eksamen.projectcalculator.domain.exception.LoginException;
+import com.eksamen.projectcalculator.domain.model.Task;
 import com.eksamen.projectcalculator.domain.model.User;
 import com.eksamen.projectcalculator.domain.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.context.request.WebRequest;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 @Controller
 public class UserController {
@@ -19,7 +25,23 @@ public class UserController {
             String password = request.getParameter("password");
             User user = USER_SERVICE.loginValid(email, password);
             request.setAttribute("user", user, WebRequest.SCOPE_SESSION);
-            return "redirect:/index";
+
+
+
+            ArrayList<Task> tasks = new ArrayList<>();
+
+            Task task1 = new Task(1L, "Rapport-skrivning", "Gruppe", null, null, "2021 11 10", "2021 12 17", 20);
+            Task task2 = new Task(2L, "Domæne model", "Gruppe", null, null, "2021 11 25", "2021 11 26", 100);
+            Task task3 = new Task(3L, "Virksomhed", "Gruppe", null, null, "2021 11 10", "2021 11 15", 20);
+
+            tasks.add(task1);
+            tasks.add(task2);
+            tasks.add(task3);
+
+
+            model.addAttribute("test", tasks);
+
+            return "index";
         } catch (LoginException e) {
             model.addAttribute("error", e.getMessage());
             return "login";
--- src/main/java/com/eksamen/projectcalculator/domain/model/Project.java
+++ src/main/java/com/eksamen/projectcalculator/domain/model/Project.java
@@ -9,71 +9,71 @@ import java.util.List;
 
 public class Project {
 
-    private Date startDate;
-    private Date deadline;
-    private String projectName;
-    private String projectDescription;
-    private List<Task> tasks;
-    private List<Project> subprojects;
+    //private Date startDate;
+    //private Date deadline;
+    //private String projectName;
+    //private String projectDescription;
+    //private List<Task> tasks;
+    //private List<Project> subprojects;
+//
+    //public Project (String projectName, String projectDescription) {
+    //    this.projectName = projectName;
+    //    this.projectDescription = projectDescription;
+    //    tasks = new ArrayList<>();
+    //    subprojects = new ArrayList<>();
+    //}
+//
+    //public void addSubproject(Project project){
+    //    subprojects.add(project);
+    //}
+//
+    //public void addTask(Task task) {
+    //    tasks.add(task);
+    //    System.out.println(tasks);
+    //}
+//
+    ////Lav unit test
+    ////samlet tidshorisont for alle tasks
+    //public double getSumOfTaskHours(){
+    //    int sum = 0;
+//
+    //    for (Task task : tasks) {
+    //        sum += task.getEstimatedHoursInTotal();
+    //    }
+    //    return sum;
+    //}
+//
+    ////omkostninger i timen for en task
+    //public double getSumOfHourlyRates(){
+    //    int sum = 0;
+//
+    //    for (Task task: tasks) {
+    //        sum += task.getSumOfHourlyRate();
+    //    }
+    //    return sum;
+    //}
+//
+    ////hvor mange gennemsnitlige daglige arbejdstimer for alle tasks i et projekt
+   //ublic double getAvgWorkHoursPrDay() {
+    //   double sum = 0;
+    //    for (int i = 0; i < tasks.size(); i++ ) {
+    //        sum += tasks.get(i).getDailyWorkHours();
+    //    }
+    //    return sum / tasks.size(); //divideret med hvor mange tasks der er
+   }//
+//
+    //public void getDeadline() {
+    //    startDate = new Date();
+    //    Calendar calendar = Calendar.getInstance();
+    //    calendar.setTime(startDate);
+    //    double endDate = getSumOfTaskHours() / getAvgWorkHoursPrDay();
+//
+    //    calendar.add(Calendar.DATE, (int)endDate);
+    //    Date deadline = calendar.getTime();
+    //    System.out.println(deadline);
+    //    System.out.println(getSumOfTaskHours());
+    //    System.out.println(getAvgWorkHoursPrDay());
+//
+//
+    //}
 
-    public Project (String projectName, String projectDescription) {
-        this.projectName = projectName;
-        this.projectDescription = projectDescription;
-        tasks = new ArrayList<>();
-        subprojects = new ArrayList<>();
-    }
-
-    public void addSubproject(Project project){
-        subprojects.add(project);
-    }
-
-    public void addTask(Task task) {
-        tasks.add(task);
-        System.out.println(tasks);
-    }
-
-    //Lav unit test
-    //samlet tidshorisont for alle tasks
-    public double getSumOfTaskHours(){
-        int sum = 0;
-
-        for (Task task : tasks) {
-            sum += task.getEstimatedHoursInTotal();
-        }
-        return sum;
-    }
-
-    //omkostninger i timen for en task
-    public double getSumOfHourlyRates(){
-        int sum = 0;
-
-        for (Task task: tasks) {
-            sum += task.getSumOfHourlyRate();
-        }
-        return sum;
-    }
-
-    //hvor mange gennemsnitlige daglige arbejdstimer for alle tasks i et projekt
-   public double getAvgWorkHoursPrDay() {
-       double sum = 0;
-        for (int i = 0; i < tasks.size(); i++ ) {
-            sum += tasks.get(i).getDailyWorkHours();
-        }
-        return sum / tasks.size(); //divideret med hvor mange tasks der er
-   }
-
-    public void getDeadline() {
-        startDate = new Date();
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(startDate);
-        double endDate = getSumOfTaskHours() / getAvgWorkHoursPrDay();
-
-        calendar.add(Calendar.DATE, (int)endDate);
-        Date deadline = calendar.getTime();
-        System.out.println(deadline);
-        System.out.println(getSumOfTaskHours());
-        System.out.println(getAvgWorkHoursPrDay());
-
-
-    }
-}

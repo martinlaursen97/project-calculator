4e25fc9797d973095aff58d5e3e0413b452b9d95
Jakob <jakobmajid00@gmail.com>
2021-11-27 19:03:12 +0100

    added notes

--- src/main/java/com/eksamen/projectcalculator/domain/exception/LoginException.java
+++ src/main/java/com/eksamen/projectcalculator/domain/exception/LoginException.java
@@ -2,6 +2,8 @@ package com.eksamen.projectcalculator.domain.exception;
 
 public class LoginException extends Exception {
 
+    // Hvad sker der her?
+
     public LoginException(String message) {
         super(message);
     }

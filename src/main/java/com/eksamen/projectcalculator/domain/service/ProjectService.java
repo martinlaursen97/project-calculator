e3122317e90101e28f914a5d07b75dbb12c32fef
martinlaursen97 <martin_laursen21@hotmail.com>
2021-12-08 19:30:02 +0100

    styles merge fix

--- src/main/java/com/eksamen/projectcalculator/domain/service/ProjectService.java
+++ src/main/java/com/eksamen/projectcalculator/domain/service/ProjectService.java
@@ -30,48 +30,7 @@ public class ProjectService {
     }
 
     public Project getProjectById(long id) {
-        Project project = FACADE.getProjectById(id);
-        if (project.getTasks() == null) project.setProjectPrice(0.0);
-
-        double sum = 0;
-        for (Task task : project.getTasks()) {
-            sum += (calculator(task.getStartDateStr(), task.getFinishDateStr()) * task.getDailyWorkHours()) * task.getPricePerHour();
-            if (task.getSubtasks() != null) {
-                for (Subtask subtask : task.getSubtasks()) {
-                    sum += (calculator(subtask.getStartDateStr(), subtask.getFinishDateStr()) * subtask.getDailyWorkHours()) * subtask.getPricePerHour();
-
-                }
-            }
-
-        }
-        project.setProjectPrice(sum);
-        System.out.println(sum);
-        return project;
-    }
-
-    public int calculator(String start, String finish) {
-        try {
-            DateFormat df = new SimpleDateFormat("yyyy MM dd");
-            Date date1 = df.parse(start);
-            Date date2 = df.parse(finish);
-            Calendar cal1 = Calendar.getInstance();
-            Calendar cal2 = Calendar.getInstance();
-            cal1.setTime(date1);
-            cal2.setTime(date2);
-
-            int numberOfDays = 0;
-            while (cal1.before(cal2)) {
-                if ((Calendar.SATURDAY != cal1.get(Calendar.DAY_OF_WEEK))
-                        && (Calendar.SUNDAY != cal1.get(Calendar.DAY_OF_WEEK))) {
-                    numberOfDays++;
-                }
-                cal1.add(Calendar.DATE, 1);
-            }
-            return numberOfDays;
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        return 0;
+        return FACADE.getProjectById(id);
     }
 
     public boolean projectIsUsers(long userId, long id) {

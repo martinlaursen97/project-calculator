386c5fb26514a0f87b6b98d1455a51bfbdf1f168
Jakob M <jakobmajid00@gmail.com>
2021-12-14 15:30:20 +0100

    UI test dosent work

--- src/test/java/com/eksamen/projectcalculator/repository/UiTest.java
+++ src/test/java/com/eksamen/projectcalculator/repository/UiTest.java
@@ -1,23 +1,29 @@
 package com.eksamen.projectcalculator.repository;
 
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.concurrent.TimeUnit;
+
 
 public class UiTest {
+
+    public static void main(String[] args) {
+        setup();
+    }
 
 
     @Test
     public static void setup() {
 
         //Load driver
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Jakob M\\Downloads");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Jakob M\\Downloads\\chromedriver_win32");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.google.dk");
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+
         driver.quit();
 
     }

905583ee8572f930221ff9f4e85c5b95406f70c8
Jakob M <jakobmajid00@gmail.com>
2021-12-14 15:06:57 +0100

    try to make ui test

--- src/test/java/com/eksamen/projectcalculator/repository/UiTest.java
+++ src/test/java/com/eksamen/projectcalculator/repository/UiTest.java
@@ -1,50 +1,27 @@
 package com.eksamen.projectcalculator.repository;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import io.github.bonigarcia.wdm.managers.ChromeDriverManager;
-import io.github.bonigarcia.wdm.webdriver.WebDriverBrowser;
+
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 
 public class UiTest {
-    private static WebDriver driver;
 
-    @BeforeAll
-    public static void setup() {
-        //Load driver
-        System.setProperty("webdriver.chrome.driver", "C:\\webdriver\\chromedriver.exe");
-        driver = new ChromeDriver();
-    }
 
     @Test
-    public void googleSearch() {
-        // Launch website
-        driver.navigate().to("localhost:8080/");
-        WebElement element = driver.findElement(By.id("password"));
-        element.click();
-        element = driver.findElement(By.name("password"));
-        element.sendKeys("123321");
-        element.submit();
+    public static void setup() {
 
-        element = driver.findElement(By.id("email"));
-        element.click();
-        element = driver.findElement(By.name("email"));
-        element.sendKeys("admin@hotmail.dk");
-        element.submit();
-
-
-        assertEquals("Cheese! - Google-søgning", driver.getTitle());
-    }
-    @AfterAll
-    public static void tearDown() {
+        //Load driver
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Jakob M\\Downloads");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.dk");
         driver.quit();
+
     }
+
+
 }
 
